bacb05dcdec1cd972f80dd53b0b192c0c6e82647
nvm027 <36326534+nvm027@users.noreply.github.com>
2018-02-15 10:38:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,3 @@
+n=int(input("enter N:"))
+for i in range(0,n):
+      print("Hello")
---
+++ leapyear.py
@@ -0,0 +1,5 @@
+year=int(input("Enter year to be checked:"))
+if(year%4==0 and year%100!=0 or year%400==0):
+    print("YES this year is a leap year!")
+else:
+    print("NO this year is not a leap year!")
