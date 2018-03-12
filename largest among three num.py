dd8442a2c35523876a9e3ebb3a7de55cf6b06612
MUGUNTHAN V <36326534+nvm027@users.noreply.github.com>
2018-03-12 17:16:21 +0530

    Update largest among three num.py

--- largest among three num.py
+++ largest among three num.py
@@ -1,6 +1,6 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+num3 = float(input("Enter third number : "))
 if (num1 > num2) and (num1 > num3):
    largest = num1
 elif (num2 > num1) and (num2 > num3):
